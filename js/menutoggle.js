116574a6188bee53f5bf84724305e9f7958c2681
stakic-igor <stakic_igor@yahoo.com>
2017-08-31 21:11:48 +0200

    modal form add

--- js/menutoggle.js
+++ js/menutoggle.js
@@ -1,15 +1,18 @@
 $('.hamburger').on('click', function() {
     document.getElementById('myCarousel').style.visibility = "hidden";
     var pageTitle = $('.sub-page__header');
+    var modal = $('.modal');
     $("#app").addClass('main-menu--active');
     $('.hero').toggleClass('hero--active');
     $(this).toggleClass('hamburger--active');
     $('#menuOpen').toggle();
     if($('.hamburger').hasClass('hamburger--active')) {
         pageTitle.show();
+        modal.hide();
     } else {
         document.getElementById('myCarousel').style.visibility = "visible";
         pageTitle.hide();
+        modal.show();
         $("#app, #touren, #tickets, #faq, #kontakt").removeClass('main-menu--active');
     }
 });
