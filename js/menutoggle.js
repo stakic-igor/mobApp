4a52db8629076087c5a48522e661fc13810b4f15
stakic-igor <stakic_igor@yahoo.com>
2017-09-01 02:29:44 +0200

    menu

--- js/menutoggle.js
+++ js/menutoggle.js
@@ -2,6 +2,7 @@ $('.hamburger').on('click', function() {
     document.getElementById('myCarousel').style.visibility = "hidden";
     var pageTitle = $('.sub-page__header');
     var modal = $('.modal-f');
+    var logoshow = $('#logoshow');
     $("#app").addClass('main-menu--active');
     $('.hero').toggleClass('hero--active');
     $(this).toggleClass('hamburger--active');
@@ -9,10 +10,12 @@ $('.hamburger').on('click', function() {
     if($('.hamburger').hasClass('hamburger--active')) {
         pageTitle.show();
         modal.hide();
+        logoshow.hide();
     } else {
         document.getElementById('myCarousel').style.visibility = "visible";
         pageTitle.hide();
         modal.show();
+        logoshow.show();
         $("#app, #touren, #tickets, #faq, #kontakt").removeClass('main-menu--active');
     }
 });
---
+++ js/modal.js
@@ -0,0 +1,6 @@
+// $(document).ready( function() {
+//     $('#modalfade').delay(3000).fadeOut();
+//   });
+// $(document).ready( function() {
+//     $('#logoshow').delay(4000).fadeIn();
+//   });
