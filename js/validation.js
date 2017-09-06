da106205e994e4e2886fe6561fc024571f1247fa
stakic-igor <stakic_igor@yahoo.com>
2017-09-06 11:59:29 +0200

    new version add

---
+++ js/validation.js
@@ -0,0 +1,57 @@
+document.forms['contact'].addEventListener('submit', function(e){
+    e.preventDefault();
+
+    var name = document.forms['contact'].name;
+    var surname = document.forms['contact'].vorname;
+    var email = document.forms['contact'].email;
+    var note = document.forms['contact'].note;
+    var error = document.getElementById('error');
+
+    //reGex
+    var string_reg = new RegExp("^([a-zA-Z ]{3,})$");
+    var mail_reg = new RegExp('^([a-z0-9](\.?[a-z0-9_-]){0,}@[a-z0-9-]+\.([a-z]{1,6}\.)?[a-z]{2,6})$')
+
+
+    var valid_name = string_reg.test(name.value);
+    var valid_surname = string_reg.test(surname.value);
+    var valid_mail = mail_reg.test(email.value);
+    
+    if( valid_name && valid_surname && valid_mail ){
+        
+        // if everything is good
+        $.ajax({
+            url: 'mail.php',
+            data:{
+                'name' : name.value,
+                'surname' : surname.value,
+                'email' : email.value,
+                'note' : note.value,
+            },
+            type: "GET",
+            success: function(result){
+                error.innerHTML = 'Nachricht wurde versendet!';
+            }
+        });
+    
+    }
+    else{
+        /*
+            error handleing
+        */
+        var error_message = '';
+
+        if(valid_name === false){
+            error_message +="Name ist ungültig.</br>"; 
+        }
+        if(valid_surname === false){
+            error_message +="Vorname ist ungültig.</br>"
+        }
+        if(valid_mail === false){
+            error_message +="E-Mail ist ungültig.</br>";
+        }
+        
+        error.innerHTML = error_message;
+    }
+
+
+});
---
+++ newsletter_sendmail.php
@@ -0,0 +1,73 @@
+<?php
+//send_email.php
+$email_from = "no-reply@fensterplatz.ch";   //Absender falls keiner angegeben wurde
+$sendermail_antwort = true;      //E-Mail Adresse des Besuchers als Absender. false= Nein ; true = Ja
+$name_von_emailfeld = "Email";   //Feld in der die Absenderadresse steht
+ 
+$empfaenger = "android@fensterplatz.ch"; //Empfänger-Adresse
+$mail_cc = ""; //CC-Adresse, diese E-Mail-Adresse bekommt einer weitere Kopie
+$betreff = "Android Newsletter Anmeldung"; //Betreff der Email
+ 
+$url_ok = "http://fensterplatz.ch/"; //Zielseite, wenn E-Mail erfolgreich versendet wurde
+$url_fehler = "http://fensterplatz.ch/"; //Zielseite, wenn E-Mail nicht gesendet werden konnte
+ 
+ 
+//Diese Felder werden nicht in der Mail stehen
+$ignore_fields = array('submit');
+ 
+ 
+ 
+ 
+//Datum, wann die Mail erstellt wurde
+$name_tag = array();
+$name_tag[0] = "Sonntag";
+$name_tag[1] = "Montag";
+$name_tag[2] = "Dienstag";
+$name_tag[3] = "Mittwoch";
+$name_tag[4] = "Donnerstag";
+$name_tag[5] = "Freitag";
+$name_tag[6] = "Samstag";
+$num_tag = date("w");
+$tag = $name_tag[$num_tag];
+$jahr = date("Y");
+$n = date("d");
+$monat = date("m");
+$time = date("H:i");
+ 
+//Erste Zeile unserer Email
+$msg = ":: Gesendet am $tag, den $n.$monat.$jahr - $time Uhr ::\n\n";
+ 
+//Hier werden alle Eingabefelder abgefragt
+while (list($name,$value) = each($_POST)) {
+   if (in_array($name, $ignore_fields)) {
+        continue; //Ignore Felder wird nicht in die Mail eingefügt
+   }
+   $msg .= "::: $name :::\n$value\n\n";
+}
+ 
+ 
+ 
+//E-Mail Adresse des Besuchers als Absender
+if ($sendermail_antwort and isset($_POST[$name_von_emailfeld]) and filter_var($_POST[$name_von_emailfeld], FILTER_VALIDATE_EMAIL)) {
+   $email_from = $_POST[$name_von_emailfeld];
+}
+ 
+$header="From: $email_from";
+ 
+if (!empty($mail_cc)) {
+   $header .= "\n";
+   $header .= "Cc: $mail_cc";
+}
+ 
+ 
+$mail_senden = mail($empfaenger,$betreff,$msg,$header);
+ 
+ 
+//Weiterleitung, hier konnte jetzt per echo auch Ausgaben stehen
+if($mail_senden){
+  header("Location: ".$url_ok); //Mail wurde gesendet
+  exit();
+} else{
+  header("Location: ".$url_fehler); //Fehler beim Senden
+  exit();
+}
